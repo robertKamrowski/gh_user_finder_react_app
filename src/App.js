0f7ff6c4e801affedd605c408620834f794cb3e8
Robert Kamrowski <robertkamrowski@MacBook-Air-Robert.local>
2021-08-31 20:04:16 +0200

    Add Glboal styles and assets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import GlobalStyles from './GlobalStyles';
 
 const App = () => {
-  return <div>app</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <h1>App</h1>
+      <h2>h2 test</h2>
+    </>
+  );
 };
 
 export default App;
---
+++ src/GlobalStyles.js
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+
+*, *::before, *::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-size: 10px;
+}
+
+h1{
+  font-family: 'Space Mono';
+  font-weight: bold;
+  font-size: 2.6rem;
+  line-height: 3.8rem;
+}
+h2{
+  font-family: 'Space Mono';
+  font-weight: bold;
+  font-size: 2.2rem;
+  line-height: 3.3rem;
+
+}
+h3{
+  font-family: 'Space Mono';
+  font-weight: normal;
+  font-size: 1.6rem;
+  line-height: 2.4rem;
+}
+h4{
+  font-family: 'Space Mono';
+  font-weight: normal;
+  font-size: 1.3rem;
+  line-height: 2rem;
+}
+body{
+  font-family: 'Space Mono';
+  font-weight: normal;
+  font-size: 1.5rem;
+  line-height: 2.5rem;
+}
+`;
+
+export default GlobalStyles;
